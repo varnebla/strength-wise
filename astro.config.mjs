166810c49a5bd7962990fd63bc0187a94089461a
Victor Arnedo Blanco <victor@MacBook-Air-de-Victor.local>
2024-09-02 00:16:21 +0200

    feat: add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import auth from "auth-astro";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue(), auth()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
 });
